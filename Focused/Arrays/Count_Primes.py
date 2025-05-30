8f0f5a8a081a999562eea74c7c6650595a06aadc
bvedpathak <bhagyesh.vedpathak@gmail.com>
2025-05-30 17:22:25 -0500

    Array Solutions

--- Focused/Arrays/Count_Primes.py
+++ Focused/Arrays/Count_Primes.py
@@ -1,1 +1,25 @@
-## Optimial Solution
+import math
+## Optimial Solution - O(n)
+def count_and_return_primes(nums):
+    result_map = {num: True for num in nums}
+    result_map[0] = False
+    result_map[1] = False
+    max_num = max(nums)
+    n = int(math.sqrt(max_num))
+    for i in range(2, n):
+        try:
+            if result_map[i]:
+                for multiple_of_i in range(i*2, max_num, i):
+                    result_map[multiple_of_i] = False
+        except KeyError:
+            print("Key Not Found")
+    # Gather the numbers that are still True (means prime) and 
+    # return them
+    list_of_primes = []
+    for k, v in result_map.items():
+        if v == True:
+            list_of_primes.append(k)
+    
+    return list_of_primes
+
+print(f"List of Primes: {count_and_return_primes([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36])} ")
---
+++ Focused/Arrays/Non_Duplicated_Number.py
@@ -0,0 +1,30 @@
+import math
+# Solution with O(n) time complexity and O(n) space complexity
+def find_single_number(nums):
+    lookup = dict()
+    for num in nums:
+        if num in lookup:
+            lookup[num] = False
+        else:
+            lookup[num] = True
+    
+    for k, v in lookup.items():
+        if v == True:
+            return k
+    return None
+
+# Similar solution with sum method 2*Sum(unique nums) - actual sum
+def find_single_number_v1(nums):
+    unique = set()
+    ideal_sum = 0
+    actual_sum = 0
+    for num in nums:
+        if num not in unique:
+            ideal_sum += 2*num
+            unique.add(num)
+        actual_sum += num
+    return ideal_sum - actual_sum;
+
+print("\n\n")
+print(f"Single Number in the given list is: {find_single_number_v1([1,2,1,2,3,11,11,4,4,5,5,7,7])}")
+print("\n\n")
