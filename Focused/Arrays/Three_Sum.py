b786205f45b66855400b5ff952e8d4d14764951b
bvedpathak <bhagyesh.vedpathak@gmail.com>
2025-06-08 23:57:43 -0400

    Array Solutions

---
+++ Focused/Arrays/Three_Sum.py
@@ -0,0 +1,24 @@
+def two_sum_v1(nums, sum):
+    lookup = dict()
+    for num in nums:
+        if sum - num in lookup:
+            return [sum - num, num]
+        lookup[num] = sum - num
+    return None
+
+# Brute Force approach
+def three_sum(nums, target):
+    lookup = dict()
+    for num in nums:
+        result = two_sum_v1(nums, target - num) 
+        if result is not None:
+            result.append(num)
+            return result
+    return None
+
+print("\n\n")
+print(three_sum([1,2,3,5], 6))
+print("\n\n")
+     
+        
+        
