16181a8513a4b18978e1b13c2751f50f26834378
bvedpathak <bhagyesh.vedpathak@gmail.com>
2025-06-22 11:56:44 -0500

    Dynamic Programming

--- EXERCISE-BST-Validate_BST.py
+++ EXERCISE-BST-Validate_BST.py
@@ -29,6 +29,13 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def print_tree(self, root, level=0, prefix="Root: "):
+        if root is not None:
+            print(" " * (level * 4) + prefix + str(root.value))
+            if root.left is not None or root.right is not None:
+                self.print_tree(root.left, level + 1, "L--- ")
+                self.print_tree(root.right, level + 1, "R--- ")
+
     def insert(self, value):
         new_node = Node(value)
         if self.root is None:
@@ -83,17 +90,41 @@ class BinarySearchTree:
             return True
         return __is_valid_bst(self.root, None)
 
+    ## This was an initial intuition one and does NOT cover a few corner cases 
+    ## so just for reference and not to use in Production
+    def validate_bst(self, root):
+        if root is None:
+            return True
+        
+        if root.left is not None and root.left.value > root.value or root.right is not None and root.right.value < root.value:
+            return False
 
+        return self.validate_bst(root.left) and self.validate_bst(root.right)
+    
+    ## This one is the optimized one and covers all the cases
+    def validate_bst_v2(self, root, min_val, max_val):
+        if root is None:
+            return True
+        
+        if min_val > root.value or max_val < root.value:
+            return False
+                
+        return self.validate_bst_v2(root.left, min_val, root.value) and self.validate_bst_v2(root.right, root.value, max_val)
+        
+    
 my_tree = BinarySearchTree()
-my_tree.right_insert(1)
+my_tree.right_insert(7)
 my_tree.right_insert(2)
-my_tree.right_insert(3)
+my_tree.right_insert(8)
 my_tree.right_insert(4)
 my_tree.right_insert(5)
+my_tree.right_insert(5)
 
+print(my_tree.print_tree(my_tree.root))
 print("First BST is valid:")
 print(my_tree.is_valid_bst())
-
+print(my_tree.validate_bst(my_tree.root))
+print(my_tree.validate_bst_v2(my_tree.root, float('-inf'), float('inf')))
 
 my_tree = BinarySearchTree()
 my_tree.insert(47)
@@ -104,8 +135,11 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
+print(my_tree.print_tree(my_tree.root))
 print("Second BST is valid:")
 print(my_tree.is_valid_bst())
+print(my_tree.validate_bst(my_tree.root))
+print(my_tree.validate_bst_v2(my_tree.root, float('-inf'), float('inf')))
 
 """
     EXPECTED OUTPUT:
