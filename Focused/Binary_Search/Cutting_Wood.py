5d1fce95fb77c43ce42d0d2708da4f2ad4148f61
bvedpathak <bhagyesh.vedpathak@gmail.com>
2025-07-08 09:53:30 -0500

    Binary Search

---
+++ Focused/Binary_Search/Cutting_Wood.py
@@ -0,0 +1,33 @@
+# Determine the highest possible setting of the woodcutter(H)
+# so that it cuts aleast k meters of wood. The hight of the trees
+# is given by the numbers in the list and a value k representing the
+# total length of wood that needs to be cut
+def highest_possible_height(trees, k):
+    if trees is None or len(trees) < 1:
+        return 0
+    # we will use Binary search on possible height settings to determine
+    # the appropriate height
+    bottom = 0
+    
+    # Find the tallest tree and store
+    top = max(trees)
+
+    # Search binary search way on different H settings
+    while bottom < top:
+        ## Bias the midpoint toward top since we need to give woods equal or
+        ## more than the k
+        mid = (bottom + top) // 2 + 1
+        wood = 0
+        for num in trees:
+            if num > mid:
+                wood += num - mid
+        if wood >= k:
+            bottom = mid
+        else:
+            top = mid - 1
+    return top
+
+print("\n")
+trees = [2, 6, 3, 8]
+k = 2
+print(f"The H should be set to {highest_possible_height(trees, k)}")
---
+++ Focused/Binary_Search/First_And_Last_Occurance_of_Number.py
@@ -0,0 +1,41 @@
+# The target many be duplicate so return the target's first and 
+# last occurance
+def first_and_last_occurance_of_a_number(nums, target):
+    lower_bound = lower_bound_binary_search(nums, target)
+    upper_bound = upper_bound_binary_search(nums, target)
+    return [lower_bound, upper_bound]
+
+def lower_bound_binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] < target:
+            left = left + 1
+        else:
+            right = mid
+    return left if nums and nums[left] == target else -1
+
+
+def upper_bound_binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        # In upper-bound binary search, bias the midpoint to the right
+        mid = (left + right) // 2 + 1
+        if nums[mid] > target:
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            left = mid
+
+    return right if nums and nums[right] == target else -1
+
+print("\n")
+nums = [1,2,3,4,4,4,5,6,7,8,9,10,11]
+target = 4
+print(f"Upper and Lower bounds for the {nums} for {target} are {first_and_last_occurance_of_a_number(nums, target)}")
